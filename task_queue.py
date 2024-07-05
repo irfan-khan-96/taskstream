6926e9518e8fa726d1e8df9072a6836732ef74b1
irfan-khan-96 <107932991+kha9-Irfa9@users.noreply.github.com>
2024-07-05 17:29:23 +0200

    started implementing task queue

---
+++ task_queue.py
@@ -0,0 +1,15 @@
+import redis
+
+class TaskQueue:
+    def __init__(self, queue_name='task_queue'):
+        self.queue_name = queue_name
+        self.redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    
+    def add_task(self, task):
+        self.redis_conn.rpush(self.queue_name, task)
+    
+    def get_task(self):
+        task = self.redis_conn.blpop(self.queue_name)
+        if task:
+            return task[1].decode('utf-8')
+        return None
